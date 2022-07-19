a400524af1142c0f9abe9fdc3a2504e1cb0881bc
Your Name <you@example.com>
2022-07-19 23:23:40 +0800

    first commit

---
+++ .vuepress/nav.js
@@ -0,0 +1,21 @@
+module.exports = [
+    {
+        text: '笔记', link: '/guide/'
+    },
+    {
+        text: '培训笔记',link:"/baodian/interview/"
+		// items: [
+        //     {text: '初级开发篇', link: '/baodian/zero/'},
+        //     {text: '中高进阶篇', link: '/baodian/high/'},
+        // ]
+    },
+    {
+        text: '施工中',link:"/baodian/talk/"
+    },
+   
+    {
+        text: 'B站首页', link: 'https://space.bilibili.com/4417416',
+
+    },
+   
+]
